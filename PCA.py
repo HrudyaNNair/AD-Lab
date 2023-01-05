ebdd400c20cc0b42301645d46535e3a55cce9d1f
HrudyaNNair <85451406+HrudyaNNair@users.noreply.github.com>
2023-01-05 12:17:13 +0530

    Update PCA.py

--- PCA.py
+++ PCA.py
@@ -1,33 +1,24 @@
+from sklearn import datasets
 import pandas as pd
-import numpy as np
+from sklearn.preprocessing import StandardScaler # to standardize the features
+from sklearn.decomposition import PCA # to apply PCA
+import seaborn as sns # to plot the heat maps
 import matplotlib.pyplot as plt
-%matplotlib inline
-from sklearn.decomposition import PCA
-from sklearn.preprocessing import StandardScaler
-#import the breast _cancer dataset
-from sklearn.datasets import load_breast_cancer
-data=load_breast_cancer()
-data.keys()
-# Check the output classes
-print(data['target_names'])
-# Check the input attributes
-print(data['feature_names'])
-# construct a dataframe using pandas
-df1=pd.DataFrame(data['data'],columns=data['feature_names'])
-# Scale data before applying PCA
-scaling=StandardScaler()
-# Use fit and transform method
-scaling.fit(df1)
-Scaled_data=scaling.transform(df1)
-# Set the n_components=3
-principal=PCA(n_components=2)
-principal.fit(Scaled_data)
-x=principal.transform(Scaled_data)
-print(x)
-# Check the dimensions of data after PCA
-print(x.shape)
-plt.figure(figsize=(10,10))
-plt.scatter(x[:,0],x[:,1],c=data['target'],cmap='plasma')
-plt.xlabel('pc1')
-plt.ylabel('pc2')
 
+iris = datasets.load_iris()
+df = pd.DataFrame(iris['data'], columns = iris['feature_names'])
+
+scalar = StandardScaler().fit_transform(df)
+scaled_data = pd.DataFrame(scalar)
+
+pca = PCA(n_components = 3).fit(scaled_data) # no. of Principal Components as 3
+data_pca = pca.transform(scaled_data)
+data_pca = pd.DataFrame(data_pca,columns=['PC1','PC2', 'PC3'])
+
+print("Correlation before PCA:\n")
+sns.heatmap(scaled_data.corr(),annot=True)
+plt.show()
+
+print("Correlation after PCA:\n")
+sns.heatmap(data_pca.corr(),annot=True)
+plt.show()
